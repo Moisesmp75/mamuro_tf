25cae61dd540e4e7f8417d5dbd04a8148c2b8e4b
Moisesmp75 <48933728+Moisesmp75@users.noreply.github.com>
2024-03-15 10:00:07 -0500

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -15,10 +15,10 @@ docker volume create enron
 docker network create enron
 
 docker pull public.ecr.aws/zinclabs/zincsearch:latest
-docker pull moisesmore75/mamuro:mamuro-app
+docker pull moisesmore75/mamuro:latest
 
 docker container run -v enron:/data -e ZINC_DATA_PATH="/data" -dp 4080:4080 -e ZINC_FIRST_ADMIN_USER=admin -e ZINC_FIRST_ADMIN_PASSWORD=Complexpass#123 --name zincsearch public.ecr.aws/zinclabs/zincsearch:latest
-docker container run -dp 3000:3000 --name mamuro-app moisesmore75/mamuro:mamuro-app
+docker container run -dp 3000:3000 --name mamuro-app moisesmore75/mamuro:latest
 
 docker network connect enron mamuro-app
 docker network connect enron zincsearch
